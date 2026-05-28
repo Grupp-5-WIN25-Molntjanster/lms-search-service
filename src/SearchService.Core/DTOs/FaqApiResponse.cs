4ba551fe9ead73f02250ee8100620630473dadfb
Alnils <Albin.nilsson.se@gmail.com>
2026-05-28 20:42:39 +0200

    created dtos for search

---
+++ src/SearchService.Core/DTOs/FaqApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchService.Core.DTOs;
+
+public record FaqApiResponse(
+    int Id,
+    string Title,
+    string Summary,
+    string Content,
+    int DisplayOrder
+);
---
+++ src/SearchService.Core/DTOs/SearchResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchService.Core.DTOs;
+
+public record SearchResult(
+    string Id,
+    string Title,
+    string Excerpt,
+    string Type,
+    string Url
+);
