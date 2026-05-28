dcc83b6e69d376a853a82f5d7bfa842cd3495f16
Alnils <Albin.nilsson.se@gmail.com>
2026-05-28 20:53:08 +0200

    added options to handle urls, token

---
+++ src/SearchService.Api/Options/FaqApiOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchService.Api.Options;
+
+public class FaqApiOptions
+{
+    public const string SectionName = "FaqApi";
+    public string BaseUrl { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}
